39d3f05a5788f2644dcd9f313ac6b1f8c5a5a4d9
ny404 <nicholas.m.yeung@gmail.com>
2020-05-06 18:19:12 -0400

    fixed parallax and added sections

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+  // Also can pass in optional settings block
+  var rellax = new Rellax('.rellax', {
+    speed: -4,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
