b0f00ce175474fda69d149184c084ac25b76aef6
ny404 <nicholas.m.yeung@gmail.com>
2020-05-06 21:11:08 -0400

    added parallax hero header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 
   // Also can pass in optional settings block
   var rellax = new Rellax('.rellax', {
-    speed: -4,
+    speed: 2,
     center: false,
     wrapper: null,
     round: true,
     vertical: true,
     horizontal: false
   });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
